56a17ba47fd86c45610d97735521ca32d4be51a2
Juan Brizuela <juanbrizuelago@gmail.com>
2023-09-04 15:17:54 -0600

    added transition to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Hero from "@/components/Hero";
-import { getProjects } from "@/sanity/sanity-utils";
+"use client";
 
-export default async function Home() {
+import Hero from "@/components/Hero";
+import Transition from "@/components/Transition";
+import { getProjects } from "@/sanity/sanity-utils";
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function Home() {
   // const projects = await getProjects();
 
   return (
-    <main className="max-w-5xl mx-auto flex max-lg:justify-center justify-start items-center h-screen -mt-6 ">
-      <Hero />
-    </main>
+    <AnimatePresence mode="wait">
+      <motion.div key={"/"} className="h-screen">
+        <Transition />
+        <main className="max-w-5xl mx-auto flex max-lg:justify-center justify-start items-center h-screen -mt-6 ">
+          <Hero />
+        </main>
+      </motion.div>
+    </AnimatePresence>
   );
 }
