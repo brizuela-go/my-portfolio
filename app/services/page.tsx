320604e6042ad65bccb9e057f52e8728d84b27b3
Juan Brizuela <juanbrizuelago@gmail.com>
2023-09-03 22:59:56 -0600

    added testimonials

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,95 @@
-export const dynamic = "auto",
-  runtime = "edge",
-  fetchCache = "auto";
+"use client";
 
-const Services = async () => {
-  return <div>Services</div>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Brain,
+  BrainCircuit,
+  Brush,
+  Lightbulb,
+  MonitorSmartphone,
+  TabletSmartphone,
+} from "lucide-react";
+
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const services = [
+  {
+    title: "Web Development",
+    description: "Full stack web development using the latest technologies",
+    icon: MonitorSmartphone,
+  },
+  {
+    title: "Mobile Development",
+    description: "For both Android and iOS using Flutter and React Native",
+    icon: TabletSmartphone,
+  },
+  {
+    title: "AI Services",
+    description:
+      "Prompt engineering, image generation, text generation, video generation, etc...",
+    icon: Brain,
+  },
+  {
+    title: "AI Development",
+    description: "Open AI, Scikit-learn, Tensorflow, PyTorch, and more...",
+    icon: BrainCircuit,
+  },
+  {
+    title: "IoT Development",
+    description: "Web and mobile apps for IoT devices and hardware coding",
+    icon: Lightbulb,
+  },
+  {
+    title: "Design",
+    description: "Adobe XD, Figma and Photoshop for UI/UX design",
+    icon: Brush,
+  },
+];
+
+const Services = () => {
+  return (
+    <main className="flex justify-center items-center h-screen max-lg:mt-20 mt-10">
+      <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 ">
+        <div className=" space-y-6 mb-10 px-4">
+          <h1 className="text-7xl font-semibold text-foreground max-lg:text-center max-lg:text-4xl ">
+            My Services
+          </h1>
+          <p className="text-muted-foreground max-lg:text-center">
+            List of services I provide. I'm fully committed to provide the best
+            quality to my clients
+          </p>
+        </div>
+
+        <Carousel
+          showArrows={false}
+          autoPlay={true}
+          infiniteLoop={true}
+          showStatus={false}
+          className="px-10"
+        >
+          {services.map((service, index) => (
+            <div className="mb-10">
+              <Card>
+                <CardHeader className="text-center flex justify-center flex-col items-center">
+                  <service.icon size={64} />
+                  <CardTitle>{service.title}</CardTitle>
+                  <CardDescription>{service.description}</CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </main>
+  );
 };
 
 export default Services;
--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -1,13 +1,91 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
-type Props = {};
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-export const dynamic = "auto",
-  runtime = "edge",
-  fetchCache = "auto";
+import Image from "next/image";
+import { Quote } from "lucide-react";
 
-const Testimonials = (props: Props) => {
-  return <div>Testimonials</div>;
+const testimonials = [
+  {
+    name: "Rafael Pérez Aguirre",
+    title: "Full Time Academic at Universidad Iberoamericana Puebla",
+    description:
+      "I have worked with Juan for more than 2 years. He is a very talented and hardworking person. He is always willing to learn new things and to help others. He is a very good team player and a very good friend. I highly recommend him.",
+    image: "/1.png",
+  },
+  {
+    name: "Huber Giron Nieto",
+    title:
+      "Computer Systems and Mechatronics Coordinator at Universidad Iberoamericana Puebla",
+    description:
+      "Talented Software Innovator, with a great capacity for teamwork and leadership.",
+    image: "/2.png",
+  },
+  {
+    name: "Antonio Benítez Ruíz",
+    title:
+      "Coordinator of the Master's Degree in Innovation and Technological Development at Universidad Iberoamericana Puebla",
+    description: "Very capable student. One of my favourites.",
+    image: "/3.png",
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <main className="mt-36 max-sm:mt-44">
+      <div className="max-w-5xl mx-auto grid grid-cols-1 gap-8 ">
+        <div className=" space-y-6 mb-10 px-4">
+          <h1 className="text-7xl font-semibold text-muted-foreground text-center max-lg:text-4xl ">
+            What people has to <span className="text-foreground"> say</span>
+          </h1>
+        </div>
+        <Carousel
+          autoPlay={true}
+          infiniteLoop={true}
+          showStatus={false}
+          className="px-10"
+        >
+          {testimonials.map((testimonial, index) => (
+            <div className="mb-10">
+              <Card>
+                <CardHeader className="text-center flex justify-center flex-col items-center">
+                  <div className="mb-6">
+                    <Image
+                      className="rounded-full h-36 w-36 "
+                      alt={testimonial.name}
+                      width={1000}
+                      height={1000}
+                      src={testimonial.image}
+                    />
+                  </div>
+
+                  <CardTitle>{testimonial.name}</CardTitle>
+                  <CardDescription>{testimonial.title}</CardDescription>
+                </CardHeader>
+                <div className="flex justify-center mb-4">
+                  <Quote className="rotate-180" size={28} />
+                </div>
+
+                <CardContent className="italic text-muted-foreground">
+                  {testimonial.description}
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </main>
+  );
 };
 
 export default Testimonials;
