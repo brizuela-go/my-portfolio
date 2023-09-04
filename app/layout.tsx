af4bcdfe30addd5833a24ff7587378933d8d103e
Juan Brizuela <juanbrizuelago@gmail.com>
2023-09-04 15:10:46 -0600

    performance test

--- app/layout.tsx
+++ app/layout.tsx
@@ -200,7 +200,7 @@ export default function RootLayout({
             <Navbar />
           </header>
           <Sidebar />
-          <ParticlesComponent />
+          {/* <ParticlesComponent /> */}
           {children}
           <Analytics />
           {/* </motion.div> */}
