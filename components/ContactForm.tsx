b30a00e0aa1ad1665aff1d7f191d11256f16a030
Juan Brizuela <juanbrizuelago@gmail.com>
2023-09-04 11:49:36 -0600

    form validation

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -9,17 +9,36 @@ import toast, { Toaster } from "react-hot-toast";
 
 export default function ContactForm() {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   async function handleSubmit(event: any) {
-    setLoading(true);
     event.preventDefault();
+
+    // Get the form data
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "f4776dce-3896-4ddb-bba9-194e23431cb7");
-
+    // Create an object from the form data
     const object = Object.fromEntries(formData);
+
+    // Check if any form field has less than 3 characters
+    const isInvalid = Object.values(object).some((value) => value.length < 5);
+
+    if (isInvalid) {
+      // Display an error toast if any field has less than 5 characters
+      toast.error("Form fields must have at least 5 characters");
+      setError(true);
+      return; // Stop form submission
+    }
+
+    // Add additional data to the form object
+    object.access_key = "f4776dce-3896-4ddb-bba9-194e23431cb7";
+
+    // Convert the object to JSON
     const json = JSON.stringify(object);
 
+    setLoading(true);
+
+    // Send the form data to the API
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
       headers: {
@@ -28,16 +47,17 @@ export default function ContactForm() {
       },
       body: json,
     });
+
     const result = await response.json();
 
     if (result.success) {
       toast.success("Message sent successfully");
-      setLoading(false);
       event.target.reset();
     } else {
       toast.error("Something went wrong, please try again");
-      setLoading(false);
     }
+
+    setLoading(false);
   }
 
   return (
@@ -45,10 +65,25 @@ export default function ContactForm() {
       <Toaster />
       <form onSubmit={handleSubmit} className="space-y-10">
         <div className="flex max-sm:flex-col justify-center gap-4">
-          <Input type="text" name="name" placeholder="Name" />
-          <Input type="email" name="email" placeholder="Email" />
+          <Input
+            className={`${error && "border-destructive"}`}
+            type="text"
+            name="name"
+            onChange={() => setError(false)}
+            placeholder="Name"
+          />
+          <Input
+            className={`${error && "border-destructive"}`}
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
         </div>
-        <Textarea name="message" placeholder="Type your message here" />
+        <Textarea
+          className={`${error && "border-destructive"}`}
+          name="message"
+          placeholder="Type your message here"
+        />
         <div className="flex justify-end gap-4">
           {loading ? (
             <Button disabled>
